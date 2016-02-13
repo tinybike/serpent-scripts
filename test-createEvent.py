80590e7d1281ba7ba2993144cd6442c6617fbece
Jack Peterson <jack@tinybike.net>
2016-02-12 23:29:08 -0800

    added createsubbranch

--- test-createEvent.py
+++ test-createEvent.py
@@ -14,18 +14,25 @@ def compile(state, filepath, gas=70000000):
 #     return ''.join(map(chr, bytearr))
 
 c = compile(init(), "test-createEvent.se")
-result = c.createMarket(
-    1010101,
-    "What will the high temperature (in degrees Fahrenheit) be in San Francisco, California, on July 1, 2016?",
-    "0x205bc01a36e2eb2",
-    "0x120000000000000000",
-    "0x51eb851eb851eb8",
-    # ["0xa4603384f73c741ba7bc5249e106009d55653ffcbbf27ceede8cc980770341d2"]
-    [1]
+# result = c.createMarket(
+#     1010101,
+#     "What will the high temperature (in degrees Fahrenheit) be in San Francisco, California, on July 1, 2016?",
+#     "0x205bc01a36e2eb2",
+#     "0x120000000000000000",
+#     "0x51eb851eb851eb8",
+#     # ["0xa4603384f73c741ba7bc5249e106009d55653ffcbbf27ceede8cc980770341d2"]
+#     [1]
+# )
+result = c.createSubbranch(
+    "radical-accelerations-56zrpywcyuv7vi",
+    100, # period length
+    1010101, # parent branch ID
+    # "0x199999999999999a", # trading fee
+    1844674407370955300,
+    0 # oracle only
 )
 
 print hex(result)
-# print hex(result)
 
 # s = tester.state()
 # tester.gas_limit = 700000000
