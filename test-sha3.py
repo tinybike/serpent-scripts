0f28a874d8d3d35d10abf311820291b5c385e835
Jack Peterson <jack@tinybike.net>
2016-05-30 01:42:52 -0700

    Updated sha3 scripts

--- test-sha3.py
+++ test-sha3.py
@@ -6,5 +6,7 @@ s = tester.state()
 c = s.abi_contract("test-sha3.se")
 
 marketID = c.makeMarketID()
+print marketID
+# print [hex(m) for m in marketID]
 print hex(marketID)
 print hex(marketID + 2**256)
