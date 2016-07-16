23c5108c0edcd4f79647030f01c461f58dbc1a2c
Jack Peterson <jack@tinybike.net>
2016-07-16 00:52:58 -0700

    Update sha3 script

--- test-sha3.py
+++ test-sha3.py
@@ -4,9 +4,10 @@ from ethereum import tester
 
 def hexify(x):
     if x > 2**255: x -= 2**256
+    if x < -2**255-1: x += 2**256
     return hex(x)[:-1]
 
 c = tester.state().abi_contract("test-sha3.se")
 
-print(hexify(c.testSha3(0xc5ae448cab9178c5736640575375a753cc217a1b2a02d103d1a5d1f275fa3cb)))
+print(hexify(c.testSha3(-0x076627fd562b1cc22a6e53ae38d5d421fb3af7fe6c1f18164d097100fba627c54)))
 print(hexify(c.makeMarketID()))
