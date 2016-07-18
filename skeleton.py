008159ae5adba30cc56bfee58b41ddd3ca344223
Jack Peterson <jack@tinybike.net>
2016-07-17 18:58:44 -0700

    Fixed hexify bounds

--- skeleton.py
+++ skeleton.py
@@ -3,16 +3,27 @@
 from ethereum import tester as t
 
 def hexify(x):
-    if x > 2**255: x -= 2**256
-    if x < -2**255-1: x += 2**256
-    return hex(x)[:-1]
+    if x > 2**255-1: x -= 2**256
+    if x < -2**255: x += 2**256
+    return hex(x)
 
 s = t.state()
 c = s.abi_contract("""
 macro ONE: 10^18
 
+macro abs($a):
+    if($a<0):
+        $a = -$a
+    $a
+
 def foo():
-    return(2^255 / ONE * 2)
+    s = 0x2bf6e5787b2a7a379f1b83efc34d454d6bb870565980280780fd16b75e943106 + 0xffffffffffffffffffffffffffffffffffffffff
+    s = sha3(s)
+    s = abs(s) / 115792089237316195423571
+    return(s)
 """)
 
-print(hexify(c.foo()))
+a = c.foo()
+
+print(a)
+print(hexify(a))
